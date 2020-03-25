4a05abd1b7129056fe74110d451d2f12a21f92e5
Costache Radu <sir_radduu@yahoo.com>
2020-03-25 19:19:45 +0200

    commit

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import my_math
 import my_string
 
 try:
-    print(my_math.is_perfect_square(
-        int(input('Introduceti numarul de verificat daca e patrat perfect: '))))
-    print(my_math.is_prime(
-        int(input('Introduceti numarul de verificat daca e prim: '))))
+    print(my_math.is_perfect_square(int(input('Introduceti numarul de verificat daca e patrat perfect: '))))
+    print(my_math.is_prime(int(input('Introduceti numarul de verificat daca e prim: '))))
 except ValueError:
     print('Acesta nu este un numar!')
 finally:
